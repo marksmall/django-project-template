920782f4e64d646018db8c1b3f218598b67b865f
Mark Small <mark.small@astrosat.space>
2018-09-11 23:16:32 +0100

    Add some common apps we will use:
    We don't necessarily use these right now, but they will come into
    play soon, CORS for cross-origin requests and Django GIS for to add
    some GIS capabilities to Django.

--- project_name/settings.py
+++ project_name/settings.py
@@ -34,6 +34,9 @@ class Common(Configuration):
         'whitenoise.runserver_nostatic',
         'django.contrib.staticfiles',
 
+        'django.contrib.gis',
+        'corsheaders',
+        'django_filters',
         'django_extensions',
         'debug_toolbar',
 
@@ -45,6 +48,7 @@ class Common(Configuration):
         'django.middleware.security.SecurityMiddleware',
         'whitenoise.middleware.WhiteNoiseMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
