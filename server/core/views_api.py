e65ecc1e48018cc52de75f7cf64f21ccbd4396bb
Mark Small <mark.small@astrosat.space>
2019-09-24 21:51:01 +0100

    feat(frontend): Get JSON properties from ENV variables
    Read and send Analytics tracking ID and Mapbox Access Token from
    environment variables.

--- server/core/views_api.py
+++ server/core/views_api.py
@@ -2,8 +2,9 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from .base import env
 
 @api_view(['GET'])
 def app_config_view(request):
     """ Return hard-coded app config """
-    return JsonResponse({ 'trackingId': 'mytrackingid', 'mapbox_token': 'mymapboxtoken' }, status=status.HTTP_200_OK)
+    return JsonResponse({ 'trackingId': env("DJANGO_TRACKING_ID", default=""), 'mapbox_token': env("DJANGO_MAPBOX_TOKEN", default="") }, status=status.HTTP_200_OK)
